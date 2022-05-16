ed9a743499d8760e8fe23ea2ae242f27b7f06fa3
mrjohnming <john.ngai@hotmail.com>
2022-05-16 14:17:22 -0400

    Import component - TravelList

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar';
+import TravelList from './TravelList';
 
 export default function App() {
   return (
     <div className="container--app">
       <Navbar />
+      <TravelList />
     </div>
-  )
+  );
 }
