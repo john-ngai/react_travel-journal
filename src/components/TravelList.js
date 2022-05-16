f45ee7ae63f0b7ef4c7c4c253ee068efc8e7aa85
mrjohnming <john.ngai@hotmail.com>
2022-05-16 15:34:01 -0400

    Replace static component with travelListItems

--- src/components/TravelList.js
+++ src/components/TravelList.js
@@ -1,9 +1,14 @@
 import TravelListItem from './TravelListItem';
+import travelData from '../data';
 
 export default function TravelList() {
+  const travelListItems = travelData.map(item => {
+    return <TravelListItem key={item.id} item={item} />
+  });
+
   return (
     <section className="container--travel-list">
-      <TravelListItem />
+      {travelListItems}
     </section>
   );
 }
