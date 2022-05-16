6a343747ffde5d4bd7b1b3a776fa153dfa14d6df
mrjohnming <john.ngai@hotmail.com>
2022-05-16 15:38:17 -0400

    Change data from static to dynamic via props

--- src/components/TravelListItem.js
+++ src/components/TravelListItem.js
@@ -1,20 +1,20 @@
 import pin from '../images/pin.png';
 
-export default function TravelListItem() {
+export default function TravelListItem(props) {
   return (
     <div className='container--travelListItem'>
-      <img src='https://source.unsplash.com/WLxQvbMyfas' alt='Travel destination' />
+      <img src={props.item.imageUrl} alt='Travel destination' />
       
       <div>
         <span className="item--location">
           <img src={pin} alt='Location pin icon' />
-          <h2>JAPAN</h2>
-          <a href='#'>View on Google Maps</a>
+          <h2>{props.item.location}</h2>
+          <a href={props.item.googleMapsUrl}>View on Google Maps</a>
         </span>
         
-        <h1 className='location--name'>Mount Fiji</h1>
-        <p className='location--dates'>12 Jan, 2021 - 24 Jan, 2021</p>
-        <p className='location--text'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        <h1 className='location--name'>{props.item.title}</h1>
+        <p className='location--dates'>{props.item.startDate} - {props.item.endDate}</p>
+        <p className='location--text'>{props.item.description}</p>
       </div>
     </div>
   );
