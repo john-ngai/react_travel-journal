a77b26d32b0b5d6077e06dbc83778fd7072ae317
mrjohnming <john.ngai@hotmail.com>
2022-05-16 14:06:37 -0400

    Remove img className

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import globe from '../images/globe.png';
 export default function Navbar() {
   return (
     <nav>
-      <img className='nav--logo' src={globe} alt='Globe icon' />
+      <img src={globe} alt='Globe icon' />
       <h3 className='nav--title'>my travel journal</h3>
     </nav>
   );
